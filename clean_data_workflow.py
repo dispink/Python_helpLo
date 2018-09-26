1ad70a71e912d1e7708409bc2580ac1097ba597c
dispink <r04224110@ntu.edu.tw>
2018-09-26 23:41:07 +0800

    This is the update of clean_data_workflow.py: 1. Change the conditions in criteria 2: 	Exclude the data if their MSE < 2 std, cps < 2 std 2. comment adjustment 3. output the cleaned data and excluded data in the same .xslx (different sheets)

--- clean_data_workflow.py
+++ clean_data_workflow.py
@@ -41,12 +41,16 @@ def claen_by_sheet(sheet_name):
     bottom_delet = int(20 / (work_sheet.position_mm[7] - work_sheet.position_mm[6]))
     work_sheet_1 = work_sheet[ : -bottom_delet]
 
-    # criteria 2: MSE < 1.5 (practicle experience), validity = 1, cps value lower than 1 std (lower limit)
-    criteria_2 = (work_sheet_1.MSE < 1.5) & (work_sheet_1.validity == 1) & (work_sheet_1.cps > (work_sheet.cps.mean() - work_sheet_1.cps.std()))
+    # criteria 2: MSE value lower than 2 std (higher limit), validity = 1, cps value lower than 2 std (lower limit)
+    criteria_2 = (
+            (work_sheet_1.MSE < (work_sheet_1.MSE.mean() + 2 * work_sheet_1.MSE.std())) 
+            & (work_sheet_1.validity == 1) 
+            & (work_sheet_1.cps > (work_sheet.cps.mean() - 2 * work_sheet_1.cps.std()))
+            )
     work_sheet_2 = work_sheet_1[criteria_2]
 
     # criteria 3: Ar/Fe lower than 1 std (upper limit)
-    # have to use the data frame pass the criteria 1 since the zero value in Fe makes statics infinite
+    # have to use the data frame pass the criteria 2 since the zero value in Fe makes statics infinite
     ArFe_ratio = work_sheet_2.Ar / work_sheet_2.Fe
     criteria_3 = ArFe_ratio < (ArFe_ratio.mean() + ArFe_ratio.std())
     work_sheet_3 = work_sheet_2[criteria_3]
@@ -58,9 +62,12 @@ def claen_by_sheet(sheet_name):
     # mark the last valid position of each core section
     end_position = work_sheet_3.iloc[-1, 0]
     
-    print(work_sheet_3)
-    work_sheet_3.to_csv('refined.csv', index = False)
-    
+    # output the cleaned data and excluded data
+    work_sheet_ex = work_sheet[~work_sheet.position_mm.isin(work_sheet_3.position_mm)]
+    writer = pd.ExcelWriter('cleaned_MD01_2419_{}.xlsx'.format(sheet_name))
+    work_sheet_3.drop('section', axis = 1).to_excel(writer,'cleaned_data', index = False)
+    work_sheet_ex.to_excel(writer,'excluded_data', index = False)
+    writer.save()
     
     return end_position
 
