71a88753700e3ced6563d947d0c794a2a5db7a1d
dispink <r04224110@ntu.edu.tw>
2018-09-28 00:17:40 +0800

    This is a major revision of clean_data_workflow.py. After discussing with Dr. Huang, the over all 15-20% data exclusion is significantly too large. The high value of MSE may introduced by the cracks but also abundance of non-selected element which possibly represents event layer (potentail highlight of the research!!!). Therefore, I remove the MSE condition in criteria 2, and lossen the condition of cps (criteria 2)and Ar/Fe (criteria 3). However, due to the removal of MSE condition, the 0 value in Fe pops up after criteria 2. This cause the infinite value of Ar/Fe. As a result, the condition of excludeing 0 in Fe is added into criteria 2. Meanwhile, in order to eveluate the effectiveness of criterias, the failed_at_criteria label is added into the excluded data sheet.

--- clean_data_workflow.py
+++ clean_data_workflow.py
@@ -20,6 +20,7 @@ file = "MD01_2419-original.xlsx"
 # load spreadsheet sec01
 xl = pd.ExcelFile(file)
 
+
 def clean_by_sheet(sheet_name):
     """It's a function for cleaning data from a given sheet.
     It automatically outputs a xlsx file containing cleaned data and excluded data.
@@ -32,7 +33,7 @@ def clean_by_sheet(sheet_name):
                           'Ar', 'K', 'Ca', 'Ti', 'Mn', 'Fe', 'Br', 'Sr']]
                          .rename(columns = {'position (mm)' : 'position_mm'})
                          )
-    
+    data_ex_map = pd.DataFrame()
     # criteria 1: exclude the last 2 cm scanning data since they may represent tape instead of core
     resolution = work_sheet.position_mm[7] - work_sheet.position_mm[6]
     bottom_delet = int(20 / resolution)
@@ -41,30 +42,35 @@ def clean_by_sheet(sheet_name):
         work_sheet_1 = work_sheet[1: -bottom_delet].copy()
     else:
         work_sheet_1 = work_sheet[ : -bottom_delet].copy()
+    data_ex_1 = work_sheet[~work_sheet.position_mm.isin(work_sheet_1.position_mm)].copy()
+    data_ex_1['failed_at_criteria'] = ['1' for _ in data_ex_1.position_mm]
 
-    # criteria 2: MSE value lower than 2 std (higher limit), validity = 1, cps value lower than 2 std (lower limit)
+    # criteria 2: validity = 1, cps value lower than 3 std (lower limit), Fe > 0
     criteria_2 = (
-            (work_sheet_1.MSE < (work_sheet_1.MSE.mean() + 2 * work_sheet_1.MSE.std())) 
-            & (work_sheet_1.validity == 1) 
-            & (work_sheet_1.cps > (work_sheet.cps.mean() - 2 * work_sheet_1.cps.std()))
+            (work_sheet_1.validity == 1) 
+            & (work_sheet_1.cps > (work_sheet.cps.mean() - 3 * work_sheet_1.cps.std()))
+            & (work_sheet_1.Fe > 0)
             )
     work_sheet_2 = work_sheet_1[criteria_2].copy()
-
-    # criteria 3: Ar/Fe lower than 1 std (upper limit)
-    # have to use the data frame pass the criteria 2 since the zero value in Fe makes statics infinite
+    data_ex_2 = work_sheet_1[~work_sheet_1.position_mm.isin(work_sheet_2.position_mm)].copy()
+    data_ex_2['failed_at_criteria'] = ['2' for _ in data_ex_2.position_mm]
+  
+    # criteria 3: Ar/Fe lower than 3 std (upper limit)
     ArFe_ratio = work_sheet_2.Ar / work_sheet_2.Fe
-    criteria_3 = ArFe_ratio < (ArFe_ratio.mean() + ArFe_ratio.std())
+    criteria_3 = ArFe_ratio < (ArFe_ratio.mean() + 3 * ArFe_ratio.std())
     work_sheet_3 = work_sheet_2[criteria_3].copy()
+    data_ex_3 = work_sheet_2[~work_sheet_2.position_mm.isin(work_sheet_3.position_mm)].copy()
+    data_ex_3['failed_at_criteria'] = ['3' for _ in data_ex_3.position_mm]
     
     # label the section name in this dataframe
     work_sheet_3['section'] = [sheet_name for i in work_sheet_3.Fe]
     
     # output the cleaned data and excluded data
-    work_sheet_ex = work_sheet[~work_sheet.position_mm.isin(work_sheet_3.position_mm)]
-    excluded_percentage = len(work_sheet_ex)/len(work_sheet) * 100
+    data_ex_map = data_ex_1.append(data_ex_2).append(data_ex_3)
+    excluded_percentage = len(data_ex_map)/len(work_sheet) * 100
     writer = pd.ExcelWriter('cleaned_MD01_2419_{}_{}%.xlsx'.format(sheet_name, round(excluded_percentage)))
     work_sheet_3.drop('section', axis = 1).to_excel(writer,'cleaned_data', index = False)
-    work_sheet_ex.to_excel(writer,'excluded_data', index = False)
+    data_ex_map.to_excel(writer,'excluded_data', index = False)
     writer.save()
     
     return work_sheet_3
